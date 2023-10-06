c1f35e3a9cf1376e1ed39b7d56cda8f9867f8570
omwami <ianalpha@protonmail.com>
2023-10-06 10:33:35 +0300

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" fn to generate pascal's triangle
+"""
+def pascal_triangle(n):
+    """ return list of lists of pascal's triangle numbers
+    """
+    if n <= 0:
+        return []
+    res = list()
+    for i in range(0, n):
+        row = list()
+        if i == 0 :
+            row.append(1)
+            res.append(row) # first element
+        else:
+            row.append(1)
+            for index, el in enumerate(res[i - 1]):
+                if index + 1 > len(res[i - 1]) - 1:
+                    # out of range
+                    row.append(1) # enclosing 1
+                else:
+                    row.append(el + res[i - 1][index + 1])
+            res.append(row)
+    return res
