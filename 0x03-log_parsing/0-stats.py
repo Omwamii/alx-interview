e1696661c97ab9e2c2c6207c62f5e9716b8247d1
omwami <ianalpha@protonmail.com>
2023-10-18 21:12:32 +0300

    solved

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,17 +6,18 @@ import sys
 count = 0
 stats = {}
 file_size = 0
+
 try:
     for line in sys.stdin:
         count += 1
         data = line.split()
-        code, f_size = data[7], int(data[8])
+        code, f_size = data[-2], int(data[-1])
         # store status codes -> count in dict & add file_size
         if not stats.get(code):
             stats[code] = 1
         else:
             stats[code] += 1  # count for number of spec status codes
-            file_size += f_size
+        file_size += f_size
 
         if count % 10 == 0:
             # print the stats
@@ -24,6 +25,8 @@ try:
             for key in sorted(stats.keys()):
                 print(f"{key}: {stats[key]}")
 except KeyboardInterrupt:
+    pass
+finally:
     print(f"File size: {file_size}")
     for key in sorted(stats.keys()):
         print(f"{key}: {stats[key]}")
