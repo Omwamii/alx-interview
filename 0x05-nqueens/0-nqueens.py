0fc4e7cf203994bbf667961ea2fe687e13e261cd
omwami <ianalpha@protonmail.com>
2023-11-24 11:28:25 +0300

    fixed board clearing

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,6 +3,13 @@
 import sys
 
 
+def clear_board(board):
+    """ clear board for a new solution to be generated """
+    for x in range(0, len(board)):
+        for y in range(0, len(board[x])):
+            board[x][y] = 0
+
+
 def diagonals_safe(x, y, board, size):
     """ check +ve and -ve diagonals for threat """
     x_diag = x - 1
@@ -37,7 +44,7 @@ def vert_safe(x, y, board):
     return True
 
 
-def n_recursive(num_queen, curr_row, board):
+def n_recursive(num_queen, curr_row, lead, board):
     """ recursively set queens on board correctly using backtracking
         num_queen: number of queens
         curr_row: current row on chess board
@@ -51,69 +58,52 @@ def n_recursive(num_queen, curr_row, board):
                 if col == 1:
                     soln.append([idx, pos])
         print(soln)
-        return
+        return True
 
     if curr_row == 0:
-        first_is_set = False
-        for item in board[0]:
-            if item == 1:
-                first_is_set = True
-        if not first_is_set:
-            # The first row was never set, first iteration
-            board[0][0] = 1  # default first position
-        else:
-            try:
-                is_next_set = board[curr_row + 1].index(1)
-            except ValueError:
-                # result of backtracking, shift position of first queen
-                for index, n in enumerate(board[0]):
-                    if index == num_queen - 1:
-                        # impossible to get soln, exit program
-                        return
-                    if n == 1:
-                        new_pos = index + 1  # shift to right
-                        board[0][index] = 0  # clear position
-                        board[0][new_pos] = 1
-                        break
-            else:
-                # likely due to another iteration to find another possible soln
-                # set everything next to 0 for fresh sln
-                set_index = board[0].index(1)
-                board[0][set_index + 1] = 1  # shift to right
-                for x in range(curr_row + 1, num_queen):
-                    for y in range(0, num_queen):
-                        board[x][y] = 0
+        board[0][lead] = 1  # default first position
+        # print(f"Iteration of [0][{lead}]")
+        return n_recursive(num_queen, curr_row + 1, lead, board)
+
     else:
-        is_set, was_set = False, False
-        for index, item in enumerate(board[curr_row]):
-            if item == 1:
-                was_set = True
-                set_index = index
+        is_set = False
+        try:
+            set_index = board[curr_row].index(1)
+        except ValueError:
+            was_set = False
+        else:
+            was_set = True
         if was_set:
             # result of backtracking
-            board[curr_row][set_index] = 0
+            # print(f"Row: {curr_row} A backtrack {board}")
+            board[curr_row][set_index] = 0  # clear the current position
             new_y = set_index + 1
             while new_y < num_queen:
                 if vert_safe(curr_row, new_y, board) and\
                         diagonals_safe(curr_row, new_y, board, num_queen):
                     board[curr_row][new_y] = 1
+                    # print(f"Set [{curr_row}][{new_y}]")
                     is_set = True
                     break
                 new_y += 1
         else:
             # first time to set or a re-set (all zeros at the time)
+            # print(f"Row {curr_row} A set or re-set (all 0) {board}")
             set_y = 0
             while set_y < num_queen:
                 if vert_safe(curr_row, set_y, board) and\
                         diagonals_safe(curr_row, set_y, board, num_queen):
                     board[curr_row][set_y] = 1
+                    # print(f"Set [{curr_row}][{set_y}]")
                     is_set = True
                     break
                 set_y += 1
         if not is_set:
             # no position was found, backtrack
-            return n_recursive(num_queen, curr_row - 1, board)
-    n_recursive(num_queen, curr_row + 1, board)
+            if curr_row - 1 == 0:
+                return False
+            return n_recursive(num_queen, curr_row - 1, lead, board)
+        return n_recursive(num_queen, curr_row + 1, lead, board)
 
 
 def nqueens():
@@ -137,17 +127,10 @@ def nqueens():
             for r in range(0, arg):
                 row.append(0)  # initialize each position to '0'
             board.append(row)
-        n_recursive(arg, 0, board)
-        try:
-            prev_lead = board[0].index(1)
-        except ValueError:
-            prev_lead = None
-        while prev_lead:
-            n_recursive(arg, 0, board)
-            new_lead = board[0].index(1)
-            if new_lead == prev_lead:  # no more solutions, exit
-                break
-            prev_lead = new_lead
+        for i in range(0, arg):
+            while n_recursive(arg, 0, i, board):
+                pass
+            clear_board(board)  # clear the board for new soln
 
 
 if __name__ == "__main__":
